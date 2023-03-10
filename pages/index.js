85eef24d1b410fb360b592963ba7474bb8454b77
Aaron Sanchez <saarong12@gmail.com>
2023-03-09 23:37:49 -0600

    Added head tag with a title element

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Material UI | Next.js Demo</title>
+      </Head>
       <main>
         <h1>Material UI & Next Demo</h1>
       </main>
