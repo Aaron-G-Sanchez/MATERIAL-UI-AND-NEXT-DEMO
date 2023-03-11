fb3a6ac7c770d2233dc06a6dc9949b96d9ccb0a0
Aaron Sanchez <saarong12@gmail.com>
2023-03-10 19:06:52 -0600

    Adjusted project structure to incorporate the app directory

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js || Material UI Demo</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
