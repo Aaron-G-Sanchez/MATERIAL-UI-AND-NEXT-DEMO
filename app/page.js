c27182844d61b623b63cf88edf64ef3890680134
Aaron Sanchez <saarong12@gmail.com>
2023-03-15 04:04:32 -0500

    Added a nav component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
+import Nav from '../components/nav'
 
 export default function Page() {
   return (
     <>
+      <Nav />
       <main className="hero">
         <div className="title-container">
-          {/* <h1 className="hero-title">WELCOME</h1> */}
+          <h1 className="hero-title">EXPORE</h1>
         </div>
       </main>
     </>
---
+++ components/nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <>
+      <nav className="nav-menu">
+        <ul className="nav">
+          <li className="nav-item">this is the nav</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
