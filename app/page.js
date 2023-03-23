0cba829e05f12ca8e37b32e1776b7973d3e96768
Aaron Sanchez <saarong12@gmail.com>
2023-03-23 08:50:56 -0500

    Created basic parallax with framer motion on the background image

--- app/page.js
+++ app/page.js
@@ -1,15 +1,22 @@
+'use client'
+import { createContext } from 'react'
 import '../styles/globals.css'
 import Nav from '../components/nav'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function Page() {
+  let { scrollYProgress } = useScroll()
+  let y = useTransform(scrollYProgress, [0, 1], ['0%', '80%'])
+
   return (
     <>
       <header className="hero-container">
         {/* <Nav /> */}
-        <img
+        <motion.img
           className="background"
           src="/assets/images/cover.jpg"
           alt="Mountains with a blue sky"
+          style={{ y }}
         />
         <h1 className="title">ELEVATE</h1>
         <img
